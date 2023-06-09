6034a2e84a66152bcdc1e311e2a77ea41f40a701
Tobias <tmauricio@homeria.com>
2023-06-09 08:57:59 +0200

    add key n and d for new and delete notas

---
+++ data/cosas_para_llevar_a_la_playa.tm
@@ -0,0 +1,9 @@
+...
+
+- juguetes de arean
+- el mate
+- lona para el piso
+- gorra y anteojos
+- protector solar
+- malla
+- flotador
--- main.py
+++ main.py
@@ -13,7 +13,12 @@ import time
 import textwrap
 
 # Archivos mios
-import keys
+import functions
+
+PATH_NOTES = '/data'
+EXTENTION_FILES = '.tm'
+INIT_PROMT_NEW_FILE = 'NUEVO - Nombre'
+INIT_PROMT_DELETE_FILE = 'ELIMINAR - Nombre'
 
 # Muestra leyendas en el footer
 def showStatus(footer_win, width, height, footer_height, promt_height, editing, cursor_y, cursor_x):
@@ -24,24 +29,48 @@ def showStatus(footer_win, width, height, footer_height, promt_height, editing, 
     if (editing):
         footer_win.addstr(1, 1, "[" + yy + ", " + xx + "]  Editando...")
     # footer_win.addstr(2, 1, "   - Flecha arriba/abajo: Navegar en el listado de notas")
-    # footer_win.addstr(3, 1, "   [Enter]: Editar nota  [Escape]: Salir y guardar  [/]: Promt")
+    footer_win.addstr(3, 1, "   [Enter]: Edit  [Escape]: Save and close  [/]: Promt [new, del]  [n]: new file  [d]: del file")
     footer_win.refresh()
 
+
 # Dibuja el prompt
-def showPromt(promt_win, cursor_prompt_y, cursor_prompt_x, text):
+def showPromt(promt_win, cursor_prompt_y, cursor_prompt_x, text, color):
     promt_win.clear()
     promt_win.border('|', '|', '-', '-', '+', '+', '+', '+')
-    promt_win.addstr(1, 1, text)
+    if (color is not None):
+        promt_win.addstr(1, 1, text, color)
+    else:
+        promt_win.addstr(1, 1, text)
     promt_win.move(cursor_prompt_y + 1, cursor_prompt_x + 1)
     promt_win.refresh()
 
+def hidePromt(promt_win):
+    promt_win.clear()
+    promt_win.refresh()
+
+
 # Genera un nuevo archivo
-def addNewFile():
-    print("generar nuevo archivo")
+def addNewFile(name):
+    name_new_file = root_dir = os.getcwd() + PATH_NOTES + "/" + name + EXTENTION_FILES
+    with open(name_new_file, "w") as newfile:
+        newfile.write("...")
+    root_dir = os.getcwd() + PATH_NOTES
+    text_files = [file for file in os.listdir(root_dir) if file.endswith(EXTENTION_FILES)]
+    return text_files
+
 
 # Remover un archivo
-def removeNewFile():
-    print("remover archivo")
+def removeNewFile(name):
+    text_files = None
+    name_new_file = root_dir = os.getcwd() + PATH_NOTES + "/" + name + EXTENTION_FILES
+    try:
+        os.remove(name_new_file)
+        root_dir = os.getcwd() + PATH_NOTES
+        text_files = [file for file in os.listdir(root_dir) if file.endswith(EXTENTION_FILES)]
+    except OSError as e:
+        print("Error al eliminar el archivo:", e)
+    return text_files
+
 
 def main(stdscr):
     curses.curs_set(0)
@@ -51,6 +80,7 @@ def main(stdscr):
     # Definir los pares de colores
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_GREEN) # Color nota seleccionada Activa
     curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_GREEN) # Color nota seleccionada Editando
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_RED) # Color error (letra - fondo)
     editing = False
     promting = False
     height, width = stdscr.getmaxyx()
@@ -79,8 +109,8 @@ def main(stdscr):
 
     editor_win = stdscr.subwin(height - footer_height - promt_height, right_width, 0, left_width + 1)
 
-    root_dir = os.getcwd() + '/data'
-    text_files = [file for file in os.listdir(root_dir) if file.endswith(".txt")]
+    root_dir = os.getcwd() + PATH_NOTES
+    text_files = [file for file in os.listdir(root_dir) if file.endswith(EXTENTION_FILES)]
 
     selected_index = 0
     # Limpio las secciones
@@ -98,6 +128,7 @@ def main(stdscr):
 
         # Busco las notas y las agrego a la izquierda
         for i, file_name in enumerate(text_files):
+            file_name = file_name[:-len(EXTENTION_FILES)]
             if i == selected_index:
                 left_win.addstr(i + 1, 1, file_name, curses.color_pair(1))
             else:
@@ -135,14 +166,20 @@ def main(stdscr):
             # Mientras escribo en el promt de abajo
             # -----------------------------------------
             while promting:
-                showPromt(promt_win, cursor_prompt_y, cursor_prompt_x, promt_text)
+                showPromt(promt_win, cursor_prompt_y, cursor_prompt_x, promt_text, None)
                 key = stdscr.getch()
 
                 if key == 27: # Codigo ASCII para la tecla Escape
                     promting = False
+                    promt_text = ""
+                    hidePromt(promt_win)
                 elif key == ord('\n'):
-                    print("acepto lo que escribo")
+                    # acepto lo que escribo
                     promting = False
+                elif key == curses.KEY_BACKSPACE or key == 127:
+                    if cursor_prompt_x > 0:
+                        cursor_prompt_x -= 1
+                        promt_text = promt_text[:cursor_prompt_x]
                 else: # Si no es una tecla de control entonces escribo sobre el contenido de la nota
                     if key >= 0 and key < 256:
                         character = chr(key)
@@ -153,31 +190,120 @@ def main(stdscr):
 
             curses.curs_set(0)
             if (len(promt_text) > 0):
-                if "new" in promt_text:
+                # TODO AGREGAR COMANDO PARA BUSCAR NOTAS
+                promt_array = promt_text.split(" ")
+                if "new" in promt_array[0]:
+                    text_files = addNewFile(promt_array[1])
+                    selected_index = 0
+                    result_promt = " Nota " + promt_array[1] + " generada"
 
-                    # AGREGAR FUNCION DE NUEVA NOTA
-                    addNewFile()
-
-                    result_promt = "Nota generada"
-                elif "del" in promt_text:
-
-                    # AGREGAR FUNCION DE ELIMINAR NOTA
-                    removeNewFile()
-
-                    result_promt = "Nota eliminada"
+                elif "del" in promt_array[0]:
+                    text_files_aux = removeNewFile(promt_array[1])
+                    selected_index = 0
+                    if (text_files_aux is not None):
+                        text_files = text_files_aux
+                        result_promt = " Nota " + promt_array[1] + " eliminada"
                 else:
-                    print("nada")
-                    result_promt = "ERROR: Comando invalido"
-                showPromt(promt_win, cursor_prompt_y, cursor_prompt_x, result_promt)
-                # promt_text = "OK"
-            # Aca analizar lo que se escribe en el promt
-            # ... TERMINAR
-        # elif key == ord('n'):
-        #     name_new_file = keys.get_new_file(stdscr)
-        #     #name_new_file = input("Nombre de la nota? ")
-        #     with open(name_new_file + ".txt", "w") as newfile:
-        #         newfile.write("Nueva nota...")
-        #         print("Nueva nota agregada", name_new_file+".txt")
+                    result_promt = " ERROR: Comando invalido"
+                showPromt(promt_win, cursor_prompt_y, cursor_prompt_x, result_promt, None)
+                curses.napms(1000)
+                hidePromt(promt_win)
+
+        # GENERAR NUEVO ARCHIVO
+        elif key == ord('n'):
+            promt_text = INIT_PROMT_NEW_FILE + ': '
+            cursor_prompt_y = 0
+            cursor_prompt_x = 16
+            promting = True
+            curses.curs_set(1)
+
+            while promting:
+                showPromt(promt_win, cursor_prompt_y, cursor_prompt_x, promt_text, None)
+                key = stdscr.getch()
+
+                if key == 27: # Codigo ASCII para la tecla Escape
+                    promting = False
+                    promt_text = ""
+                    hidePromt(promt_win)
+                elif key == ord('\n'):
+                    # acepto lo que escribo
+                    promting = False
+                elif key == curses.KEY_BACKSPACE or key == 127:
+                    if cursor_prompt_x > 0:
+                        cursor_prompt_x -= 1
+                        promt_text = promt_text[:cursor_prompt_x]
+                else: # Si no es una tecla de control entonces escribo sobre el contenido de la nota
+                    if key >= 0 and key < 256:
+                        character = chr(key)
+                        if (character == ' '):
+                            character = '_'
+                        if character.isprintable():
+                            promt_text = promt_text[:cursor_prompt_x] + character
+                            cursor_prompt_x += 1
+            curses.curs_set(0)
+            if (len(promt_text) > 0):
+                promt_array = promt_text.split(": ")
+                color = curses.color_pair(1)
+                if (promt_array[1]):
+                    text_files = addNewFile(promt_array[1])
+                    selected_index = 0
+                    result_promt = " Nota " + promt_array[1] + " generada"
+                else:
+                    color = curses.color_pair(3)
+                    result_promt = " ERROR: Archivo no generado"
+                showPromt(promt_win, cursor_prompt_y, cursor_prompt_x, result_promt, color)
+                curses.napms(1000)
+                hidePromt(promt_win)
+
+
+        # ELIMINAR ARCHIVO
+        elif key == ord('d'):
+            promt_text = INIT_PROMT_DELETE_FILE + ': ' + selected_file[:-len(EXTENTION_FILES)]
+            cursor_prompt_y = 0
+            cursor_prompt_x = 19
+            promting = True
+            curses.curs_set(1)
+
+            while promting:
+                showPromt(promt_win, cursor_prompt_y, cursor_prompt_x, promt_text, None)
+                key = stdscr.getch()
+
+                if key == 27: # Codigo ASCII para la tecla Escape
+                    promting = False
+                    promt_text = ""
+                    hidePromt(promt_win)
+                elif key == ord('\n'):
+                    # acepto lo que escribo
+                    promting = False
+                elif key == curses.KEY_BACKSPACE or key == 127:
+                    if cursor_prompt_x > 0:
+                        cursor_prompt_x -= 1
+                        promt_text = promt_text[:cursor_prompt_x]
+                else: # Si no es una tecla de control entonces escribo sobre el contenido de la nota
+                    if key >= 0 and key < 256:
+                        character = chr(key)
+                        if (character == ' '):
+                            character = '_'
+                        if character.isprintable():
+                            promt_text = promt_text[:cursor_prompt_x] + character
+                            cursor_prompt_x += 1
+            curses.curs_set(0)
+            if (len(promt_text) > 0):
+                promt_array = promt_text.split(": ")
+                color = curses.color_pair(1)
+                if (promt_array[1]):
+                    text_files_aux = removeNewFile(promt_array[1])
+                    selected_index = 0
+                    if (text_files_aux is not None):
+                        text_files = text_files_aux
+                        result_promt = " Nota " + promt_array[1] + " eliminada"
+                else:
+                    color = curses.color_pair(3)
+                    result_promt = " ERROR: Comando invalido"
+                showPromt(promt_win, cursor_prompt_y, cursor_prompt_x, result_promt, color)
+                curses.napms(1000)
+                hidePromt(promt_win)
+
 
         elif key == curses.KEY_ENTER or key == ord('\n'):
             # Cambio de color el cursor
@@ -258,7 +384,16 @@ def main(stdscr):
                         character = chr(key)
                         if character.isprintable():
                             editor_text_lines[cursor_y] = editor_text_lines[cursor_y][:cursor_x] + character + editor_text_lines[cursor_y][cursor_x:]
-                            cursor_x += 1
+                            if (cursor_x == right_width - 2):
+                                if cursor_y < editor_text_height - 1:
+                                    new_line = editor_text_lines[cursor_y][cursor_x:]
+                                    editor_text_lines[cursor_y] = editor_text_lines[cursor_y][:cursor_x]
+                                    editor_text_lines.insert(cursor_y + 1, new_line)
+                                    editor_text_height += 1
+                                    cursor_y += 1
+                                    cursor_x = 1
+                            else:
+                                cursor_x += 1
 
                 # Actualizo estados footer
                 showStatus(footer_win, width, height, footer_height, promt_height, editing, cursor_y, cursor_x)
